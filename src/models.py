8e8f52072b6f1585b063e663996c287487079a18
Sussanara <sussanaracely@gmail.com>
2022-08-18 22:40:07 +0000

    Insta

--- src/models.py
+++ src/models.py
@@ -5,52 +5,58 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
-
 Base = declarative_base()
-
-class Favorites(Base):
-    __tablename__= 'favorites'
-    id = Column(Integer,primary_key=True)
-    planet_id = Column(Integer,ForeignKey('planets.id'))
-    character_id = Column(Integer,ForeignKey('characters.id'))
-    vehicle_id = Column(Integer,ForeignKey('vehicles.id'))
-    user_id = Column(Integer,ForeignKey('user.id'))
 
 class User(Base):
     __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     username = Column(String(250))
-    password = Column(String(250))
-    email = Column(String(250), unique=True)
-
-class Characters(Base):
-    __tablename__ = 'characters'
-    id = Column(Integer, primary_key=True)
     name = Column(String(250))
-    height = Column(String(250))
-    mass = Column(String(250))
-    gender = Column(String(250))
-    vehicle_id = Column(Integer,ForeignKey('vehicles.id'))
+    biography = Column(String(250))
+    email = Column(String(250), ForeignKey('person.id'))
+    followers = relationship('Follower')
+    post = relationship('Post')
+    comment = relationship('Comment')
 
-class Planets(Base):
-    __tablename__ = 'planets'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    gravity = Column(Integer)
-    terrain = Column(String(250))
-    population = Column(Integer)
+class Follower(Base):
+    __tablename__ = 'follower'
+    ignore = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer,ForeignKey('user.id'))
+    user_to_id = Column(Integer,ForeignKey('user.id'))
 
-class Vehicles(Base):
-    __tablename__= 'vehicles'
+class Post(Base):
+    __tablename__= 'post'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    cost = Column(Integer)
-    model = Column(String(250))
-    pilots = Column(Integer)
-    max_speed = Column(Integer)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    title = Column(String(250))
+    description = Column(String(250))
+    likes = Column(Integer)
+    post = relationship('Media')
+    comment = relationship('Comment')
+
+class Comment(Base):
+    __tablename__= 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Media(Base):
+    __tablename__= 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    src_url = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))        
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
